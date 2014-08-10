8a2f99815a5686051391951d4cd771999484960e
rhythmthumper <github3200@jaredbowie.com>
2014-08-10 08:08:28 +0700

    nremoved version 1 files

--- src/thaiocr/test555.clj
+++
@@ -1,9 +0,0 @@
-(ns thaiocr.test555
-  (:require [thaiocr.test555 :refer :all]
-            [clojure.test :refer :all]))
-
-(defn play-with []
-  (let [some-map {:keya {:keyb {:keyc {:keyd 5555}}}}]
-    (flatten some-map)
-    )
-  )
